ff740a8db742af44c057bb3288c5d473c65ee77a
Marat Sayfutdinov <saymon.161@gmail.com>
2018-09-03 21:20:43 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+let webpack = require('webpack');
+let HtmlPlugin = require('html-webpack-plugin');
+let CleanWebpackPlugin = require('clean-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let rules = require('./webpack.config.rules')();
+let path = require('path');
+
+rules.push({
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+    })
+});
+
+module.exports = {
+    entry: {
+        friendsfilter: './src/friendsfilter.js'
+    },
+    devServer: {
+        index: 'friendsfilter.html'
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('dist')
+    },
+    devtool: 'source-map',
+    module: { rules },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                drop_debugger: false,
+                warnings: false
+            }
+        }),
+        new ExtractTextPlugin('styles.css'),
+        new HtmlPlugin({
+            title: 'Другофильтр',
+            template: 'friendsfilter.hbs',
+            filename: 'friendsfilter.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+};
